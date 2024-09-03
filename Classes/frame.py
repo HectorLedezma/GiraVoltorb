aa813217a7d46bdd54a7232c43af385baf36d7ea
HectorLedezma <hector.ledezma@alumnos.ucentral.cl>
2024-09-02 21:24:04 -0400

    Se añadió marcador y mensajes de informacion

--- Classes/frame.py
+++ Classes/frame.py
@@ -1,22 +1,63 @@
 import tkinter as tk
 from PIL import ImageTk, Image
 import random as rng
-
+from tkinter import messagebox,simpledialog
+import sys
+import time
 from Classes.casilla import Casilla
 from Classes.indicador import Indicador
 
 class Frame:
     def __init__(self):
+
+        self.fichasGanadas = 1
+        while True:
+            self.fichasInGame = simpledialog.askinteger("Apuesta","¿Cuantas fichas deseas apostar?",initialvalue=0)
+            if self.fichasInGame != None:
+                if self.fichasInGame > self.fichasGanadas:
+                    messagebox.showinfo("Espera","No puedes apostar mas fichas de las que tienes")
+                elif self.fichasInGame == 0:
+                    messagebox.showinfo("Espera","Debes apostar almenos una ficha")
+                else:
+                    break
+            else:
+                sys.exit()
+
+        
+        self.fichasGanadas = self.fichasGanadas - self.fichasInGame
+        self.fichasMeta = 1
+
         self.ventana = tk.Tk()
+        self.ventana.config(bg="blue")
         self.rows, self.cols = 5, 5
+        
+        self.TopFrame = tk.Frame(self.ventana,bg="blue")
+        self.TopFrame.grid(row=0, column=0, padx=20, pady=10)
+
+        self.MarcadorFrame = tk.Frame(self.TopFrame,bg="cyan")
+        self.MarcadorFrame.grid(row=0,column=0)
+
         self.CasillasFrame = tk.Frame(self.ventana)
-        self.CasillasFrame.grid(row=0, column=0, padx=20, pady=20)
+        self.CasillasFrame.config(bg="blue")
+        self.CasillasFrame.grid(row=0, column=1, padx=20, pady=20)
+        
+        #self.BottomFrame = tk.Frame(self.ventana)
+        #self.BottomFrame.grid(row=1, column=0, padx=20, pady=10)
+
         self.values = []
         self.slots = []
         self.icons = []
         self.positions = []
         self.cargado = False
+
         
+        self.marcadorGanadas = tk.Label(self.MarcadorFrame, text=f"{self.fichasGanadas}",bg="cyan",fg="black",font=("Arial", 16, "bold"))
+        self.marcadorInGame = tk.Label(self.MarcadorFrame, text=f"{self.fichasInGame}",bg="cyan",fg="black",font=("Arial", 16, "bold"))
+        self.boton = tk.Label(self.TopFrame, text="Abandonar",pady=10,padx=40,bg="red",fg="white",font=("Arial", 18, "bold"))
+
+        self.win = False
+
+
     def show(self):        
         print("iniciando bucle")
         self.ventana.title("Gira Voltorb")
@@ -25,22 +66,34 @@ class Frame:
         ancho_pantalla = self.ventana.winfo_screenwidth() #método para obtener Ancho
         alto_pantalla = self.ventana.winfo_screenheight() #método para obtener Alto
         # Calcular las coordenadas para centrar la ventana
-        ancho_ventana = 600
+        ancho_ventana = 900
         alto_ventana = 600
         posicion_x = (ancho_pantalla - ancho_ventana) // 2 
         posicion_y = (alto_pantalla - alto_ventana) // 2
 
         # Establecer el tamaño y la posición de la ventana
         self.ventana.geometry(f"{ancho_ventana}x{alto_ventana}+{posicion_x}+{posicion_y}")
-
+        self.ventana.resizable(False, False)
         # Dimensiones de la matriz
         
         # Cargar las imágenes de los íconos
         self.slots = self.NewGame()
+        self.CalcMeta()
         self.draw_matrix()
+        """Marcador"""
+        tk.Label(self.MarcadorFrame, text="Fichas ganadas",fg="black",bg="cyan",font=("Arial", 18, "bold")).grid(row=0, column=0,padx=10)
+        tk.Label(self.MarcadorFrame, text="Fichas en juego",fg="black",bg="cyan",font=("Arial", 18, "bold")).grid(row=2, column=0,padx=10)
+        self.marcadorGanadas.grid(row=1, column=0)
+        self.marcadorInGame.grid(row=3, column=0)
+        
+        
+        """Botones"""
+        
+        self.boton.grid(row=5, column=0)
 
         self.ventana.grid_rowconfigure(0, weight=1)
         self.ventana.grid_columnconfigure(0, weight=1)
+
         self.ventana.mainloop()
         
     def NewGame(self):
@@ -48,7 +101,7 @@ class Frame:
         self.cargado = False
         slots = []
         self.values = []
-        ratio = [0,0,0,0,1,1,1,1,1,1,1,1,1,2,3]
+        ratio = [0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,2,3]
         for i in range(0, self.rows * self.cols):
             n = rng.choice(ratio)        
             slot = Casilla(valor=n)
@@ -74,8 +127,7 @@ class Frame:
         """Redibuja la matriz de íconos en el Frame"""
         
         for widget in self.CasillasFrame.winfo_children():
-            if(widget.winfo_class() != "Frame"):
-                widget.destroy()  # Limpiar los widgets anteriores 
+            widget.destroy()  # Limpiar los widgets anteriores 
         
         
         #print(f"largo children = {len(self.CasillasFrame.winfo_children())}")
@@ -97,27 +149,53 @@ class Frame:
                 self.positions.append(label)
                 valuesMatx[i].append(self.values[i * self.cols + j])
 
-        if not self.cargado:
-            valuesX, valuesY = suma_ejes(valuesMatx)
-            voltorbX, voltorbY = cuenta_voltorb(valuesMatx)
-            for i in range(self.rows):
-                for j in range(self.cols):#valuesMatx[n][m] j
-                    labelX = Indicador(self.CasillasFrame,total=valuesY[j],bombs=voltorbY[j])#tk.Label(self.CasillasFrame,bg="red",text=f"{valuesY[j]}")
-                    labelX.grid(row=self.rows,column=j, padx=10, pady=10)
-                labelY = Indicador(self.CasillasFrame,total=valuesX[i],bombs=voltorbX[i])#tk.Label(self.CasillasFrame,bg="green",text=f"{valuesX[i]}")
-                labelY.grid(row=i,column=self.cols, padx=10, pady=10)
-            self.cargado = True
+    
+        valuesX, valuesY = suma_ejes(valuesMatx)
+        voltorbX, voltorbY = cuenta_voltorb(valuesMatx)
+        for i in range(self.rows):
+            for j in range(self.cols):#valuesMatx[n][m] j
+                labelX = Indicador(self.CasillasFrame,total=valuesY[j],bombs=voltorbY[j])#tk.Label(self.CasillasFrame,bg="red",text=f"{valuesY[j]}")
+                labelX.grid(row=self.rows,column=j, padx=10, pady=10)
+            labelY = Indicador(self.CasillasFrame,total=valuesX[i],bombs=voltorbX[i])#tk.Label(self.CasillasFrame,bg="green",text=f"{valuesX[i]}")
+            labelY.grid(row=i,column=self.cols, padx=10, pady=10)
+        
         
         
     def update(self,event,pos):
         self.slots[pos].voltear()
         imgroute = f"Images/values/{self.slots[pos].valor}.png"
         
+        self.fichasInGame = self.fichasInGame * self.slots[pos].valor
+
+        self.marcadorInGame.config(text=f"{self.fichasInGame}")
+
         img = Image.open(imgroute)
         img = img.resize((50, 50), Image.ADAPTIVE)  # Redimensionar la imagen
         icon = ImageTk.PhotoImage(img)
         self.icons[pos] = icon
         self.positions[pos].config(image=self.icons[pos])
+        print(self.fichasInGame,self.fichasMeta)
+        if(self.fichasInGame == self.fichasMeta):
+            print("Has Ganado")
+            self.fichasGanadas = self.fichasInGame
+            self.marcadorGanadas.config(text=f"{self.fichasGanadas}")
+            continuar = messagebox.askyesno("Felicidades","¡¡¡HAS GANADO!!!\n¿Deseas continuar?")
+            if continuar:
+                self.reset()
+            else:
+                sys.exit()
+        elif self.fichasInGame == 0:
+                print("GAME OVER")
+                continuar = messagebox.askyesno("Lastima","has peridido\n¿Deseas continuar?")
+                if continuar:
+                    if self.fichasGanadas == 0:
+                        self.fichasGanadas = 1
+                        self.marcadorGanadas.config(text=f"{self.fichasGanadas}")
+                        messagebox.showinfo("","Has recibido una ficha para continuar")
+                    self.reset()
+                else:
+                    sys.exit()
+
     
     def GetImage(self,pos):
         imgroute = "Images/SuperBall.png"
@@ -126,3 +204,25 @@ class Frame:
         else:
             imgroute = f'Images/SuperBall.png'
         return Image.open(imgroute)
+    
+    def CalcMeta(self):
+        self.fichasMeta = 1
+        for c in self.slots:
+            if c.valor != 0:
+                self.fichasMeta = self.fichasMeta * c.valor
+
+    def reset(self):
+        while True:
+            self.fichasInGame = simpledialog.askinteger("Apuesta","¿Cuantas fichas deseas apostar?")
+            if self.fichasInGame > self.fichasGanadas:
+                messagebox.askquestion("Espera","No puedes apostar mas fichas de las que tienes")
+            elif self.fichasInGame == 0:
+                messagebox.askquestion("Espera","Debes apostar almenos una ficha")
+            else:
+                break
+        self.fichasGanadas = self.fichasGanadas - self.fichasInGame
+        self.marcadorGanadas.config(text=f"{self.fichasGanadas}")
+        self.marcadorInGame.config(text=f"{self.fichasInGame}")
+        self.slots = self.NewGame()
+        self.CalcMeta()
+        self.draw_matrix()
